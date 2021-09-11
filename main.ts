c1a9701af28d9abd6e0167ccfa983ede62a81d01
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2021-09-11 16:44:50 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("TEKNISK")
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "TEKNISK") {
+        OrchestraMusician.send(1, "BOB")
+        OrchestraMusician.send(1, "TIM")
+        basic.pause(StepLength)
+    }
+})
+let StepLength = 0
+StepLength = 200
